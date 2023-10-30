42c071bc64ed94da4d294ff8fd14f474459bc234
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2023-10-30 20:07:11 +0700

    Hutao

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from Playable import Player
 from pygame import mixer
 
-#Ricardo is noobie boobie Uwie Joobie Dickie
+#Ricardo is noobie boobie Uwie Joobie Dickie หมาๆ
 mixer.init()
 pygame.init()
 
